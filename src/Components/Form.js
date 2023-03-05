357706ba486511f09b31b2138e47b11a9d9bdda7
Sarrveshhh <sarrvesharyaa@gmail.com>
2023-03-05 23:03:01 +0530

    added nothing

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -5,6 +5,7 @@ const Form = () => {
   const [itemName, setItemName] = useState('');
   const [sellerName, setSellerName] = useState('');
   const [quantity, setquantity] = useState('');
+  const [price, setPrice] = useState('');
 
   return (
     <div className="create">
@@ -18,16 +19,24 @@ const Form = () => {
           onChange={(e) => setItemName(e.target.value)}
         />
         <label>Sellers Name</label>
-        <input
+        <input type="text"
           required
           value={sellerName}
           onChange={(e) => setSellerName(e.target.value)}
         ></input>
         <label>Product's Quantity</label>
         <input
+        type="text"
         required
         value={quantity}
         onChange={(e) => setquantity(e.target.value)}          
+        ></input>
+        <label>Product's Price</label>
+        <input
+        type="number    "
+        required
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}          
         ></input>
         <button>Add Item</button>
       </form>
--- src/data/navbars.js
+++ src/data/navbars.js
@@ -1,6 +1,6 @@
 const topNav = [
     { id: 1, label: "Add Products", href: "#" },
-    { id: 2, label: "Predict", href: "#" },
+    { id: 2, label: "Predict", href: "/seller/  predict" },
     { id: 3, label: "Previous Product", href: "#" },
   ];
   
