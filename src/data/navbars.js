ce5208e9163d4954748c0c96aa7260cd07149699
Sarrveshhh <sarrvesharyaa@gmail.com>
2023-03-05 23:23:09 +0530

    Added routing for all elements of navbar

--- src/data/navbars.js
+++ src/data/navbars.js
@@ -1,6 +1,7 @@
 const topNav = [
-    { id: 1, label: "Add Products", href: "#" },
-    { id: 2, label: "Predict", href: "#" },
+    { id: 1, label: "Home", href: "/seller" },
+    { id: 1, label: "Add Products", href: "/seller/addP" },
+    { id: 2, label: "Predict", href: "/seller/predict" },
     { id: 3, label: "Previous Product", href: "#" },
   ];
   
