9ddb33dd4e1124369a379de7402ff0b5f155c41f
Mandar Prakash Kulkarni <mandar4567@gmail.com>
2023-04-27 23:15:07 +0530

    signout functionality completed

--- src/Components/Signout.js
+++ src/Components/Signout.js
@@ -14,12 +14,12 @@ const Signout = () => {
         navigate('/');
     }
   return (
-    <div>
-      <div className='flex justify-center flex-col items-center'>
-        <div>Are you sure you want to sign out?</div>
-        <div>
-        <button className='border-solid border-2' onClick={SignOut}>Yes</button>  
-        <button className='border-solid border-2' onClick={notSignOut}>No</button>
+    <div className=''>
+      <div className='flex flex-col mx-auto mt-40 bg-green-200 border shadow-2xl rounded-xl w-1/2 h-96'>
+        <div className='mx-auto mt-20 font-bold text-3xl'>Are you sure you want to sign out?</div>
+        <div className='mx-auto mt-20 '>
+        <button className='border-solid h-10  w-40 mt-2 mx-auto   bg-green-400 rounded-md' onClick={SignOut}>Yes</button>  
+        <button className='border-solid h-10  w-40 mt-2 mx-auto ml-6 bg-red-400 rounded-md' onClick={notSignOut}>No</button>
         </div>
       </div>
     </div>
